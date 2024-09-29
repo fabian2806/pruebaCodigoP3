85bf74f7db68413177473dd62f746b866e068383
Fabián <fabianmr2806@hotmail.com>
2024-09-28 20:50:25 -0500

    commit4

--- SoftRhPrendasDA/src/pe/edu/pucp/softrh/prendas/daoimp/PrendaDAOImplementacion.java
+++ SoftRhPrendasDA/src/pe/edu/pucp/softrh/prendas/daoimp/PrendaDAOImplementacion.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package pe.edu.pucp.softrh.prendas.daoimp;
-
+//hola
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
