a6f35a354f05dc3c7db3b96fc118a5927659cb08
osorizado <plmarca@pucp.edu.pe>
2024-10-01 20:53:09 -0500

    Pagina web

---
+++ SoftRhWeb/SoftRhWeb/Content/JavaScript.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const cartButtons = document.querySelectorAll('.product button');
+
+    cartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Producto añadido al carrito');
+        });
+    });
+});
